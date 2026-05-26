fb15bf2a2889151b7472613f0c7e21dc893279ba
VicMasterGS <vicente.garay02@inacapmail.cl>
2026-05-25 22:09:36 -0400

    primer commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.getElementById("formulario").addEventListener("submit",function(event){ //Detecta y controla cuando el usuario intenta enviar el formulario
+let errors = [] //Cuenta los errores
+let nombre = document.getElementById("nombre").value; //Lee y guarda el texto escrito
+let apellido = document.getElementById("apellido").value;
+let cargo = document.getElementById("cargo").value;
+let email = document.getElementById("email").value;
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
